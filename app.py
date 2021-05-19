10092f7d75455159d48ab340412841c1fbeba8cd
Valeriya Ilina <59905206+Valeriya-Ilina@users.noreply.github.com>
2021-05-18 22:41:26 -0400

    initial setup, install requirements

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+DEBUG=True
+PORT=8000
+
+app = Flask(__name__)
+
+app.secret_key = os.environ.get("FLASK_APP_SECRET")
+
+@app.route('/')
+def hello():
+    return 'Hello, Wardrobe!'
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=DEBUG, port=PORT)
