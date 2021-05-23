cbb065747a2ba5c2cf1347c9e84a993875631141
Valeriya Ilina <59905206+Valeriya-Ilina@users.noreply.github.com>
2021-05-22 21:39:18 -0400

    install cors

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from resources.items import items
 from resources.categories import categories
 from resources.outfits import outfits
 from resources.users import users
+from flask_cors import CORS
 from flask_login import LoginManager
 import os
 from dotenv import load_dotenv
@@ -21,11 +22,16 @@ login_manager.init_app(app)
 def load_user(user_id):
     return models.User_Account.get(models.User_Account.id == user_id)
 
+CORS(items, origins=['http://localhost:3000'], supports_credentials=True)
+CORS(users, origins=['http://localhost:3000'], supports_credentials=True)
+CORS(categories, origins=['http://localhost:3000'], supports_credentials=True)
+CORS(outfits, origins=['http://localhost:3000'], supports_credentials=True)
 
 app.register_blueprint(items, url_prefix='/api/v1/items')
 app.register_blueprint(categories, url_prefix='/api/v1/categories')
 app.register_blueprint(outfits, url_prefix='/api/v1/outfits')
 app.register_blueprint(users, url_prefix='/api/v1/users')
+
 
 @app.route('/')
 def hello():
